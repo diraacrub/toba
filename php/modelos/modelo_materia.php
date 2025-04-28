b17690f2878efde557e15889f1e14e736b9814ce
root <root@inst-builder-debian-12-build-build-psbhv.us-central1-a.c.gce-image-builder.internal>
2025-04-28 23:17:09 +0000

    fix api materias

--- php/modelos/modelo_materia.php
+++ php/modelos/modelo_materia.php
@@ -1,218 +1,139 @@
 <?php
+/**
+ * Modelo simplificado para operaciones con materias
+ */
 class modelo_materia {
+    // Campos que permiten búsqueda (incluimos todos los campos del JSON)
+    static $campos_busqueda = array(
+        'id_materia', 'nombre_carrera', 'nombre_materia', 'ano_plan', 'periodo_plan',
+        'horas_totales', 'horas_semanales', 'depto_principal', 'depto', 'area',
+        'orientacion', 'contenidos_minimos', 'correlativas_para_cursar',
+        'correlativas_para_aprobar', 'competencias', 'optativa', 'trayecto',
+        'cod_carrera', 'plan_guarani', 'version_guarani', 'plan_mocovi',
+        'plan_ordenanzas', 'cod_guarani', 'observaciones'
+    );
     
-    // Helper: elimina acentos y maneja caracteres Unicode
-    static function removeAccents($string) {
-        if (!is_string($string)) {
-            return $string;
-        }
-        
-        // Asegurar que estamos trabajando con UTF-8
-        if (!mb_check_encoding($string, 'UTF-8')) {
-            $string = mb_convert_encoding($string, 'UTF-8');
-        }
-        
-        // Mapa de caracteres Unicode con acentos a sus equivalentes sin acentos
-        $unwanted_array = array(
-            'Á'=>'A', 'À'=>'A', 'Â'=>'A', 'Ä'=>'A', 'Ã'=>'A',
-            'á'=>'a', 'à'=>'a', 'â'=>'a', 'ä'=>'a', 'ã'=>'a',
-            'É'=>'E', 'È'=>'E', 'Ê'=>'E', 'Ë'=>'E',
-            'é'=>'e', 'è'=>'e', 'ê'=>'e', 'ë'=>'e',
-            'Í'=>'I', 'Ì'=>'I', 'Î'=>'I', 'Ï'=>'I',
-            'í'=>'i', 'ì'=>'i', 'î'=>'i', 'ï'=>'i',
-            'Ó'=>'O', 'Ò'=>'O', 'Ô'=>'O', 'Ö'=>'O', 'Õ'=>'O',
-            'ó'=>'o', 'ò'=>'o', 'ô'=>'o', 'ö'=>'o', 'õ'=>'o',
-            'Ú'=>'U', 'Ù'=>'U', 'Û'=>'U', 'Ü'=>'U',
-            'ú'=>'u', 'ù'=>'u', 'û'=>'u', 'ü'=>'u',
-            'Ñ'=>'N', 'ñ'=>'n',
-            // Caracteres Unicode adicionales representados en formato JSON como \uXXXX
-            "\u00C1"=>'A', "\u00C0"=>'A', "\u00C2"=>'A', "\u00C4"=>'A', "\u00C3"=>'A',
-            "\u00E1"=>'a', "\u00E0"=>'a', "\u00E2"=>'a', "\u00E4"=>'a', "\u00E3"=>'a',
-            "\u00C9"=>'E', "\u00C8"=>'E', "\u00CA"=>'E', "\u00CB"=>'E',
-            "\u00E9"=>'e', "\u00E8"=>'e', "\u00EA"=>'e', "\u00EB"=>'e',
-            "\u00CD"=>'I', "\u00CC"=>'I', "\u00CE"=>'I', "\u00CF"=>'I',
-            "\u00ED"=>'i', "\u00EC"=>'i', "\u00EE"=>'i', "\u00EF"=>'i',
-            "\u00D3"=>'O', "\u00D2"=>'O', "\u00D4"=>'O', "\u00D6"=>'O', "\u00D5"=>'O',
-            "\u00F3"=>'o', "\u00F2"=>'o', "\u00F4"=>'o', "\u00F6"=>'o', "\u00F5"=>'o',
-            "\u00DA"=>'U', "\u00D9"=>'U', "\u00DB"=>'U', "\u00DC"=>'U',
-            "\u00FA"=>'u', "\u00F9"=>'u', "\u00FB"=>'u', "\u00FC"=>'u',
-            "\u00D1"=>'N', "\u00F1"=>'n'
-        );
-        
-        // Si estamos tratando con los códigos de escapado JSON (\uXXXX)
-        if (strpos($string, '\u') !== false) {
-            // Convertir \uXXXX a caracteres reales
-            $string = preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($matches) {
-                return mb_convert_encoding(pack('H*', $matches[1]), 'UTF-8', 'UTF-16BE');
-            }, $string);
-        }
-        
-        return strtr($string, $unwanted_array);
-    }
-
-    // Función para traer todas las materias 
+    // Define campos que contienen texto con posibles acentos
+    static $campos_texto = array(
+        'nombre_carrera', 'nombre_materia', 'depto_principal', 'depto', 'area',
+        'orientacion', 'contenidos_minimos', 'correlativas_para_cursar',
+        'correlativas_para_aprobar', 'competencias', 'trayecto', 'observaciones'
+    );
+    
+    /**
+     * Función para traer todas las materias 
+     */
     static function get_all_materias() {
-        $sql = "SELECT * FROM materias";
-        return toba::db()->consultar($sql);
+        $sql = "SELECT * FROM huayca.materias";
+        $materias = toba::db()->consultar($sql);
+        
+        // Debug: imprimir información sobre la primera materia
+        if (!empty($materias)) {
+            error_log("[DEBUG] Total de materias obtenidas: " . count($materias));
+            error_log("[DEBUG] Primera materia: " . json_encode($materias[0], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+            error_log("[DEBUG] Tipo de valor del campo depto: " . gettype($materias[0]['depto']));
+            error_log("[DEBUG] Bytes hexadecimales del depto: " . bin2hex($materias[0]['depto']));
+            error_log("[DEBUG] Encoding del depto: " . mb_detect_encoding($materias[0]['depto'], 'UTF-8, ISO-8859-1'));
+        } else {
+            error_log("[DEBUG] No se encontraron materias");
+        }
+        
+        return $materias;
     }
 
-    // Función para traer una materia por su ID
+    /**
+     * Función para traer una materia por su ID
+     */
     static function get_materia_by_id($id_materia) {
-        $sql = "SELECT * FROM materias WHERE id_materia = " . quote($id_materia);
+        $sql = "SELECT * FROM huayca.materias WHERE id_materia = " . quote($id_materia);
         return toba::db()->consultar_fila($sql);
     }
-
-    // Función para traer varias materias dada una lista de IDs
-    static function get_materias_by_ids($ids) {
-        $ids = array_map('intval', $ids);
-        $lista_ids = implode(",", $ids);
-        $sql = "SELECT * FROM materias WHERE id_materia IN ($lista_ids)";
-        return toba::db()->consultar($sql);
-    }
     
-    // Método para filtrar materias según diversos criterios
-    static function get_materias_by_filters($filters, $ids = null) {
-        $conditions = array();
-          // Definimos la cadena de acentos a quitar y sus equivalentes
-        $from = 'áàâäãéèêëíìîïóòôöõúùûüñÁÀÂÄÃÉÈÊËÍÌÎÏÓÒÔÖÕÚÙÛÜÑ';
-        $to   = 'aaaaaeeeeiiiioooouuuunAAAAAEEEEIIIIOOOOOUUUUN';
-        
-        // Filtro por departamento
-        if (isset($filters['departamento'])) {
-            $val = self::removeAccents(mb_strtolower($filters['departamento'], 'UTF-8'));
-            $val_sql = toba::db()->quote($val);
-            $conditions[] = "(translate(lower(depto_principal), '$from', '$to') = $val_sql OR translate(lower(depto), '$from', '$to') = $val_sql)";
+    /**
+     * Convierte un valor de UTF-8 a ISO-8859-1 para consultas en BD
+     */
+    private static function utf8_to_latin1($valor) {
+        if (is_string($valor) && mb_detect_encoding($valor, 'UTF-8', true)) {
+            return utf8_decode($valor);
         }
+        return $valor;
+    }
+      /**
+     * Buscar materias con múltiples filtros combinables
+     * Búsqueda case-insensitive y accent-insensitive
+     * Permite usar _exact al final de un parámetro para buscar coincidencia exacta
+     */
+    static function buscar_materias($filtros = array()) {
+        $where = array();
+        $params = array();
         
-        // Filtro específico por depto_principal
-        if (isset($filters['depto_principal'])) {
-            $val = self::removeAccents(mb_strtolower($filters['depto_principal'], 'UTF-8'));
-            $val_sql = toba::db()->quote($val);
-            $conditions[] = "translate(lower(depto_principal), '$from', '$to') = $val_sql";
-        }
-
-        // Filtro específico por depto 
-        if (isset($filters['depto'])) {
-            $val = self::removeAccents(mb_strtolower($filters['depto'], 'UTF-8'));
-            $val_sql = toba::db()->quote($val);
-            $conditions[] = "translate(lower(depto), '$from', '$to') = $val_sql";
-        }
+        error_log("[DEBUG] Filtros recibidos: " . json_encode($filtros, JSON_UNESCAPED_UNICODE));
         
-        // Filtro por área
-        if (isset($filters['area'])) {
-            $val = self::removeAccents(mb_strtolower($filters['area'], 'UTF-8'));
-            $val_sql = toba::db()->quote($val);
-            $conditions[] = "translate(lower(area), '$from', '$to') = $val_sql";
-        }
-        
-        // Filtro por orientación
-        if (isset($filters['orientacion'])) {
-            $val = self::removeAccents(mb_strtolower($filters['orientacion'], 'UTF-8'));
-            $val_sql = toba::db()->quote($val);
-            $conditions[] = "translate(lower(orientacion), '$from', '$to') = $val_sql";
-        }
-        
-        // Filtro para comprobar si contenidos_mínimos está vacío o no
-        if (isset($filters['contenidos_minimos_empty']) && $filters['contenidos_minimos_empty'] === 'true') {
-            $conditions[] = "(contenidos_minimos IS NULL OR TRIM(contenidos_minimos) = '')";
-        } else if (isset($filters['contenidos_minimos_empty']) && $filters['contenidos_minimos_empty'] === 'false') {
-            $conditions[] = "(contenidos_minimos IS NOT NULL AND TRIM(contenidos_minimos) <> '')";
-        }
-        
-        // Búsqueda por contenido en contenidos_mínimos
-        if (isset($filters['contenidos_minimos'])) {
-            $search = trim($filters['contenidos_minimos']);
-            $words = preg_split('/\s+/', $search);
-            foreach ($words as $word) {
-                $word_proc = self::removeAccents(mb_strtolower($word, 'UTF-8'));
-                $word_sql = toba::db()->quote('%'.$word_proc.'%');
-                $conditions[] = "translate(lower(contenidos_minimos), '$from', '$to') LIKE $word_sql";
+        // Convertir filtros de UTF-8 a ISO-8859-1 para coincidir con la BD
+        foreach ($filtros as $campo => $valor) {
+            // Verificar si es una búsqueda exacta (campo termina con _exact)
+            $busqueda_exacta = false;
+            if (substr($campo, -6) === '_exact') {
+                $campo_real = substr($campo, 0, -6);
+                $busqueda_exacta = true;
+            } else {
+                $campo_real = $campo;
+            }
+              if (in_array($campo_real, self::$campos_busqueda)) {
+                $valor_latin1 = self::utf8_to_latin1($valor);
+                
+                // Campos que siempre deben usar coincidencia exacta (pero case-insensitive)
+                $campos_exactos = array('depto', 'area', 'orientacion');
+                
+                if (in_array($campo_real, self::$campos_texto)) {
+                    // Si es un campo de coincidencia exacta o se especificó _exact
+                    if ($busqueda_exacta || in_array($campo_real, $campos_exactos)) {
+                        // Búsqueda exacta pero case-insensitive para campos de texto
+                        $valor_sql = toba::db()->quote($valor_latin1);
+                        $where[] = "UPPER($campo_real) = UPPER($valor_sql)";
+                        error_log("[DEBUG] Filtro EXACTO para campo '$campo_real': UPPER($campo_real) = UPPER($valor_sql)");
+                    } else {
+                        // Búsqueda parcial para el resto de campos de texto
+                        $valor_sql = toba::db()->quote('%' . $valor_latin1 . '%');
+                        $where[] = "UPPER($campo_real) LIKE UPPER($valor_sql)";
+                        error_log("[DEBUG] Filtro PARCIAL para campo '$campo_real': UPPER($campo_real) LIKE UPPER($valor_sql)");
+                    }
+                } else {
+                    // Para campos numéricos o códigos, siempre usamos igualdad exacta
+                    $valor_sql = toba::db()->quote($valor_latin1);
+                    $where[] = "$campo_real = $valor_sql";
+                    error_log("[DEBUG] Filtro para campo '$campo_real': $campo_real = $valor_sql");
+                }
             }
         }
-
-        // Filtro por nombre de materia
-        if (isset($filters['nombre_materia'])) {
-            $val = self::removeAccents(mb_strtolower($filters['nombre_materia'], 'UTF-8'));
-            $val_sql = toba::db()->quote("%".$val."%");
-            $conditions[] = "translate(lower(nombre_materia), '$from', '$to') LIKE $val_sql";
-        }
-
-        // Filtro por nombre de carrera
-        if (isset($filters['nombre_carrera'])) {
-            $val = self::removeAccents(mb_strtolower($filters['nombre_carrera'], 'UTF-8'));
-            $val_sql = toba::db()->quote("%".$val."%");
-            $conditions[] = "translate(lower(nombre_carrera), '$from', '$to') LIKE $val_sql";
-        }
-
-        // Filtro por año del plan
-        if (isset($filters['ano_plan'])) {
-            $val = intval($filters['ano_plan']);
-            $conditions[] = "ano_plan = $val";
-        }
         
-        // Filtro por periodo del plan
-        if (isset($filters['periodo_plan'])) {
-            $val = self::removeAccents(mb_strtolower($filters['periodo_plan'], 'UTF-8'));
-            $val_sql = toba::db()->quote($val);
-            $conditions[] = "translate(lower(periodo_plan), '$from', '$to') = $val_sql";
+        try {
+            // Construir la consulta SQL
+            $sql = "SELECT * FROM huayca.materias";
+            
+            if (!empty($where)) {
+                $sql .= " WHERE " . implode(" AND ", $where);
+            }
+            
+            error_log("[DEBUG] SQL generado: $sql");
+            
+            // Ejecutar la consulta
+            $resultado = toba::db()->consultar($sql);
+            error_log("[DEBUG] Total de resultados: " . count($resultado));
+            
+            return $resultado;
+            
+        } catch (Exception $e) {
+            error_log("[ERROR] Error en consulta SQL: " . $e->getMessage());
+            error_log("[ERROR] Stack trace: " . $e->getTraceAsString());
+            return array();
         }
-
-        // Filtro por horas totales
-        if (isset($filters['horas_totales'])) {
-            $val = intval($filters['horas_totales']);
-            $conditions[] = "horas_totales = $val";
-        }
-
-        // Filtro por horas semanales
-        if (isset($filters['horas_semanales'])) {
-            $val = intval($filters['horas_semanales']);
-            $conditions[] = "horas_semanales = $val";
-        }
-
-        // Filtro por optativa
-        if (isset($filters['optativa'])) {
-            $val = self::removeAccents(mb_strtolower($filters['optativa'], 'UTF-8'));
-            $val_sql = toba::db()->quote($val);
-            $conditions[] = "translate(lower(optativa), '$from', '$to') = $val_sql";
-        }
-        
-        // Filtro por trayecto
-        if (isset($filters['trayecto'])) {
-            $val = self::removeAccents(mb_strtolower($filters['trayecto'], 'UTF-8'));
-            $val_sql = toba::db()->quote($val);
-            $conditions[] = "translate(lower(trayecto), '$from', '$to') = $val_sql";
-        }
-
-        // Filtro por código de carrera
-        if (isset($filters['cod_carrera'])) {
-            $val = self::removeAccents(mb_strtolower($filters['cod_carrera'], 'UTF-8'));
-            $val_sql = toba::db()->quote($val);
-            $conditions[] = "translate(lower(cod_carrera), '$from', '$to') = $val_sql";
-        }
-
-        // Filtro por plan_guarani
-        if (isset($filters['plan_guarani'])) {
-            $val = self::removeAccents(mb_strtolower($filters['plan_guarani'], 'UTF-8'));
-            $val_sql = toba::db()->quote($val);
-            $conditions[] = "translate(lower(plan_guarani), '$from', '$to') = $val_sql";
-        }
-
-        // Filtro por código guaraní
-        if (isset($filters['cod_guarani'])) {
-            $val = self::removeAccents(mb_strtolower($filters['cod_guarani'], 'UTF-8'));
-            $val_sql = toba::db()->quote($val);
-            $conditions[] = "translate(lower(cod_guarani), '$from', '$to') = $val_sql";
-        }
-
-        // Filtrar por IDs específicos
-        if ($ids) {
-            $ids = array_map('intval', $ids);
-            $lista_ids = implode(",", $ids);
-            $conditions[] = "id_materia IN ($lista_ids)";
-        }
-
-        $where = count($conditions) ? "WHERE " . implode(" AND ", $conditions) : "";
-        $sql = "SELECT * FROM materias $where";
-        return toba::db()->consultar($sql);
+    }
+    
+    /**
+     * Función para traer materias por departamento (mantenida por compatibilidad)
+     */
+    static function get_materias_by_depto($depto) {
+        return self::buscar_materias(array('depto' => $depto));
     }
 }
--- php/rest/recurso_materias.php
+++ php/rest/recurso_materias.php
@@ -2,103 +2,109 @@
 use SIUToba\rest\rest;
 use SIUToba\rest\lib\modelable;
 
-// Se incluye el modelo. La ruta es relativa: retrocede un directorio y entra a "modelos"
-require_once __DIR__ . '/../modelos/modelo_materia.php';
+require_once __DIR__ . '../../modelos/modelo_materia.php';
 
 /**
  * @description Operaciones sobre Materias
+ * 
+ * Permite búsquedas combinadas por cualquier campo:
+ *  - Campos generales de texto (búsqueda parcial case-insensitive): ?campo=valor
+ *    Ejemplo: materias?nombre_materia=biolog&contenidos_minimos=evolución
+ * 
+ *  - Campos depto, area y orientacion (búsqueda exacta case-insensitive): ?campo=valor
+ *    Ejemplo: materias?depto=ECOLOGÍA&orientacion=EVOLUCIÓN
+ *    Estos campos siempre usan coincidencia exacta, no parcial.
+ * 
+ *  - Para forzar búsquedas exactas en cualquier campo: ?campo_exact=valor
+ *    Ejemplo: materias?nombre_materia_exact=BIOLOGIA GENERAL
+ * 
+ * Todos los campos son buscables: id_materia, nombre_carrera, nombre_materia,
+ * ano_plan, periodo_plan, horas_totales, horas_semanales, depto_principal,
+ * depto, area, orientacion, etc.
  */
 class recurso_materias implements modelable
 {
-    // Método documentativo para la generación de modelos (no obligatorio para el funcionamiento)
-    static function _get_modelos(){
+    // Método documentativo para la generación de modelos
+    static function _get_modelos()
+    {
         return array();
-    }    // Método para obtener la lista de materias.
-    // Permite filtrar por:
-    // - id_materia (parámetro "ids")
-    // - departamento (búsqueda en depto_principal y depto)
-    // - depto_principal (búsqueda específica en depto_principal)
-    // - depto (búsqueda específica en depto)
-    // - area (búsqueda en área)
-    // - orientacion (búsqueda en orientación)
-    // - contenidos_minimos_empty (true/false para verificar si contenidos_minimos está vacío o no)
-    // - contenidos_minimos (búsqueda flexible en contenidos_minimos, insensible a mayúsculas, espacios, tildes)
-    // - nombre_materia (búsqueda con LIKE, insensible a mayúsculas, espacios, tildes)
-    // - nombre_carrera (búsqueda con LIKE)
-    // - ano_plan (valor exacto)
-    // - periodo_plan (valor exacto)
-    // - horas_totales (valor exacto)
-    // - horas_semanales (valor exacto)
-    // - optativa (valor exacto)
-    // - trayecto (valor exacto)
-    // - cod_carrera (valor exacto)
-    // - plan_guarani (valor exacto)
-    // - cod_guarani (búsqueda exacta)
-    static function get_list(){
-        // Parámetros de filtrado
-        $ids_param = isset($_GET['ids']) ? $_GET['ids'] : null;
+    }    /**
+     * Obtiene la lista de materias.
+     * Permite filtrar por cualquier campo combinable, con búsqueda case y accent insensitive
+     * Soporta búsqueda exacta añadiendo _exact al nombre del parámetro
+     */
+    static function get_list() 
+    {
+        // Log request details
+        error_log("[DEBUG] Solicitud recibida: " . $_SERVER['REQUEST_URI']);
+        error_log("[DEBUG] Query string: " . $_SERVER['QUERY_STRING']);
         
-        // Filtros específicos para materias
-        $departamento = isset($_GET['departamento']) ? $_GET['departamento'] : null;
-        $depto_principal = isset($_GET['depto_principal']) ? $_GET['depto_principal'] : null;
-        $depto = isset($_GET['depto']) ? $_GET['depto'] : null;
-        $area = isset($_GET['area']) ? $_GET['area'] : null;
-        $orientacion = isset($_GET['orientacion']) ? $_GET['orientacion'] : null;
-        $contenidos_minimos_empty = isset($_GET['contenidos_minimos_empty']) ? $_GET['contenidos_minimos_empty'] : null;
-        $contenidos_minimos = isset($_GET['contenidos_minimos']) ? $_GET['contenidos_minimos'] : null;
-        $nombre_materia = isset($_GET['nombre_materia']) ? $_GET['nombre_materia'] : null;
-        $nombre_carrera = isset($_GET['nombre_carrera']) ? $_GET['nombre_carrera'] : null;
-        $ano_plan = isset($_GET['ano_plan']) ? $_GET['ano_plan'] : null;
-        $periodo_plan = isset($_GET['periodo_plan']) ? $_GET['periodo_plan'] : null;
-        $horas_totales = isset($_GET['horas_totales']) ? $_GET['horas_totales'] : null;
-        $horas_semanales = isset($_GET['horas_semanales']) ? $_GET['horas_semanales'] : null;
-        $optativa = isset($_GET['optativa']) ? $_GET['optativa'] : null;
-        $trayecto = isset($_GET['trayecto']) ? $_GET['trayecto'] : null;
-        $cod_carrera = isset($_GET['cod_carrera']) ? $_GET['cod_carrera'] : null;
-        $plan_guarani = isset($_GET['plan_guarani']) ? $_GET['plan_guarani'] : null;
-        $cod_guarani = isset($_GET['cod_guarani']) ? $_GET['cod_guarani'] : null;
-
-        // Armar arreglo de filtros (solo los que tienen valor)
-        $filters = array();
-        if ($departamento)           $filters['departamento'] = $departamento;
-        if ($depto_principal)        $filters['depto_principal'] = $depto_principal;
-        if ($depto)                  $filters['depto'] = $depto;
-        if ($area)                   $filters['area'] = $area;
-        if ($orientacion)            $filters['orientacion'] = $orientacion;
-        if ($contenidos_minimos_empty !== null) $filters['contenidos_minimos_empty'] = $contenidos_minimos_empty;
-        if ($contenidos_minimos)     $filters['contenidos_minimos'] = $contenidos_minimos;
-        if ($nombre_materia)         $filters['nombre_materia'] = $nombre_materia;
-        if ($nombre_carrera)         $filters['nombre_carrera'] = $nombre_carrera;
-        if ($ano_plan)               $filters['ano_plan'] = $ano_plan;
-        if ($periodo_plan)           $filters['periodo_plan'] = $periodo_plan;
-        if ($horas_totales)          $filters['horas_totales'] = $horas_totales;
-        if ($horas_semanales)        $filters['horas_semanales'] = $horas_semanales;
-        if ($optativa)               $filters['optativa'] = $optativa;
-        if ($trayecto)               $filters['trayecto'] = $trayecto;
-        if ($cod_carrera)            $filters['cod_carrera'] = $cod_carrera;
-        if ($plan_guarani)           $filters['plan_guarani'] = $plan_guarani;
-        if ($cod_guarani)            $filters['cod_guarani'] = $cod_guarani;
-
-        // Si hay filtros o IDs específicos, usamos el método de filtrado
-        if(!empty($filters) || $ids_param){
-            if($ids_param){
-                $ids = explode(',', $ids_param);
-                $materias = modelo_materia::get_materias_by_filters($filters, $ids);
+        // Recolectar todos los parámetros de búsqueda
+        $filtros = array();
+        
+        // Procesar parámetros normales
+        foreach (modelo_materia::$campos_busqueda as $campo) {
+            // Parámetro normal (búsqueda parcial)
+            if (isset($_GET[$campo])) {
+                $filtros[$campo] = urldecode($_GET[$campo]);
+                error_log("[DEBUG] Filtro parcial '$campo': '" . $filtros[$campo] . "'");
+            }
+            
+            // Parámetro con _exact (búsqueda exacta)
+            $param_exact = $campo . '_exact';
+            if (isset($_GET[$param_exact])) {
+                $filtros[$param_exact] = urldecode($_GET[$param_exact]);
+                error_log("[DEBUG] Filtro exacto '$param_exact': '" . $filtros[$param_exact] . "'");
+            }
+        }
+        
+        // Determinar si debemos buscar con filtros o traer todo
+        if (!empty($filtros)) {
+            error_log("[DEBUG] Buscando con filtros: " . json_encode($filtros, JSON_UNESCAPED_UNICODE));
+            
+            // Buscar materias con los filtros proporcionados
+            $materias = modelo_materia::buscar_materias($filtros);
+            
+            // Verificar resultados
+            if (empty($materias)) {
+                error_log("[DEBUG] Los filtros no devolvieron resultados");
             } else {
-                $materias = modelo_materia::get_materias_by_filters($filters);
+                error_log("[DEBUG] Los filtros devolvieron " . count($materias) . " resultados");
             }
         } else {
-            // Sin filtros adicionales, obtenemos todas las materias
+            // Sin filtros, obtener todas las materias
             $materias = modelo_materia::get_all_materias();
+            error_log("[DEBUG] Consulta sin filtros devolvió " . count($materias) . " materias");
         }
-
-        rest::response()->get($materias);
+        
+        // Convertir los resultados de ISO-8859-1 a UTF-8 para la respuesta JSON
+        foreach ($materias as &$materia) {
+            foreach (modelo_materia::$campos_texto as $campo) {
+                if (isset($materia[$campo]) && !mb_check_encoding($materia[$campo], 'UTF-8')) {
+                    $materia[$campo] = utf8_encode($materia[$campo]);
+                }
+            }
+        }
+        
+        // Enviar respuesta
+        rest::response()->get($materias, 200, JSON_UNESCAPED_UNICODE);
     }
-
-    // Método para obtener una materia en particular por su ID
-    // Se invoca, por ejemplo, con GET /rest/materias/{id}
-    static function get($id){
+      /**
+     * Obtiene una materia por su ID
+     */
+    static function get($id) 
+    {
         $materia = modelo_materia::get_materia_by_id($id);
-        rest::response()->get($materia);
+        
+        // Convertir campos de texto de ISO-8859-1 a UTF-8
+        if ($materia) {
+            foreach (modelo_materia::$campos_texto as $campo) {
+                if (isset($materia[$campo]) && !mb_check_encoding($materia[$campo], 'UTF-8')) {
+                    $materia[$campo] = utf8_encode($materia[$campo]);
+                }
+            }
+        }
+        
+        rest::response()->get($materia, 200, JSON_UNESCAPED_UNICODE);
     }
 }
